16120d90fd9b4733705a3c97ae44769bbba18677
Jan Singon Slany <singond@seznam.cz>
2023-04-03 22:21:10 +0200

    05: Split script into functions

--- 05/tomography.jl
+++ 05/tomography.jl
@@ -3,32 +3,37 @@ using DelimitedFiles
 using FFTW
 using ImageView
 
+function backproject(proj::Matrix{Float64}, N::Integer)
+    x = range(-1, 1, length=N)
+    y = range(-1, 1, length=N)
+    ns = size(proj,1)
+    img = zeros(N, N)
+    for (k, xk) in enumerate(x), (l, yl) in enumerate(y), (m, ϕm) in enumerate(ϕ)
+        ξ = xk * cos(ϕm) + yl * sin(ϕm)
+        idx = round(Int, ns*(ξ+1)/2)
+        if 1 < idx < ns
+            img[l,k] += proj[idx,m]
+        end
+    end
+    img
+end
+
+function filter_projection(proj::Matrix{Float64}, filter::Vector{Float64})
+    filtered = similar(proj)
+    for (c, p) in enumerate(eachcol(proj))
+        pt = fft(p)
+        pt = pt .* filter
+        filtered[:,c] = real(ifft(pt))
+    end
+    filtered
+end
+
 ϕ = readdlm("CAT/projections_phi.txt")
 proj = readdlm("CAT/projections_pxp.txt")
 
 #ω = fftfreq(size(proj,1), size(proj,1)/2)
 ω = fftfreq(size(proj,1))
 filter = abs.(ω)
-filtered = similar(proj)
-for (c, p) in enumerate(eachcol(proj))
-    pt = fft(p)
-    pt = pt .* filter
-    filtered[:,c] = real(ifft(pt))
-end
-
-N = 400
-x = range(-1, 1, length=N)
-y = range(-1, 1, length=N)
-
-img = zeros(N, N)
-for (k, xk) in enumerate(x), (l, yl) in enumerate(y), (m, ϕm) in enumerate(ϕ)
-    ξ = xk * cos(ϕm) + yl * sin(ϕm)
-    if -1 <= ξ <= 1
-        idx = round(Int, size(filtered, 1)*(ξ+1)/2)
-        if 1 < idx
-        img[l,k] += filtered[idx,m]
-        end
-    end
-end
+img = backproject(filter_projection(proj, filter), 400)
 
 imshow(img, flipy=true)
