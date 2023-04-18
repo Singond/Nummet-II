1d7a56a8d9a220feed7219365f8742085e7378fe
Jan Slany <jan.slany@mail.muni.cz>
2023-04-18 17:08:03 +0200

    10: Reimplement hypersphere in Julia

---
+++ 10/hypersphere.jl
@@ -0,0 +1,32 @@
+using LinearAlgebra
+
+using Plots
+using SpecialFunctions
+
+"""
+Estimate the volume of a unit hypersphere of dimension dim
+using Monte Carlo integration.
+"""
+function hypersphere_mc(dim, N)
+    S = 0
+    for k = 1:N
+        x = 2 * rand(dim) .- 1
+        S += norm(x) < 1
+    end
+    2^dim * S / N
+end
+
+"""
+Return the volume of a unit hypersphere of dimension dim.
+"""
+hypersphere_exact(dim) = 2pi^(dim / 2) / (dim * gamma(dim / 2))
+
+Dmax = 16
+N = 1e6
+
+D = 1:Dmax
+V = [hypersphere_mc(dim, N) for dim in D]
+
+scatter(D, V)
+dd = hypersphere_exact.(D)
+plot!(hypersphere_exact)
