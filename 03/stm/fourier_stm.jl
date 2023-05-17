83024c5c988235565f492ec8b032099895800c6b
Jan Slany <jan.slany@mail.muni.cz>
2023-05-17 18:01:26 +0200

    03: Align Fourier image to frequencies

--- 03/stm/fourier_stm.jl
+++ 03/stm/fourier_stm.jl
@@ -4,5 +4,8 @@ using ImageView
 
 img = Gray.(load("stm-graphene.png"))
 c = fft(gray.(img))
+fx = fftfreq(size(c, 2))
+fy = fftfreq(size(c, 1))
+cc = c[sortperm(fy), sortperm(fx)]
 
-imshow(abs.(c), CLim(0,1000))
+imshow(abs.(cc), CLim(0,1000))
